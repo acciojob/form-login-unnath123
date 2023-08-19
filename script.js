60f871336271e5380070e3d40dad8a42e8e3b21c
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-19 23:23:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	let arr=document.querySelectorAll("input");
+	let str1=arr[0].value;
+	let str2= arr[1].value;
 
+	alert(str1+" "+str2);
+	
 }
+
+
